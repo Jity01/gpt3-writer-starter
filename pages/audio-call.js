23bd6d147b1f2eede69b9823210e6b8ae4cf00b9
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2023-04-23 02:40:24 -0400

    implement speech recognition along with its ui

--- pages/audio-call.js
+++ pages/audio-call.js
@@ -1,11 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import Button from '../lib/button/button';
 import { getConversationGeneration, createFullChatContext } from '../utils/client/prompt-helpers';
 import { useSpeechSynthesis } from 'react-speech-kit';
 import Layout from '../lib/layout/layout';
 import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill';
+import Title from '../lib/title/title';
+import Root from '../lib/root/root';
 
 const appId = '703efb7d-6930-4a9e-b71c-2282c3d7d145';
 const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
@@ -13,16 +15,84 @@ SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
 
 // todo: let user pick out the voice & the rate (peep their demo & use react-speech-kit)
 // todo: add a place where the user can input how they felt about the session (abstract textarea?)
+
 // todo: insert session into database
 
 function AudioCall() {
-  const [isGenerating, setIsGenerating] = useState(false);
-  const { speak, cancel, speaking, supported, voices } = useSpeechSynthesis();
-  const englishVoices = voices.filter(voice => voice.lang.includes('en'));
   const [chat, setChat] = useState([]);
   const [conversationStatus, setConversationStatus] = useState('not-started');
-  const { transcript, listening, browserSupportsSpeechRecognition, isMicrophoneAvailable } = useSpeechRecognition();
+  const [stopListeningFlag, setStopListeningFlag] = useState(false);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const {
+    transcript,
+    resetTranscript,
+    listening,
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable
+  } = useSpeechRecognition();
+  const {
+      speak,
+      cancel,
+      speaking,
+      supported,
+      voices
+    } = useSpeechSynthesis();
+  const englishVoices = voices.filter(voice => voice.lang.includes('en'));
+
+  const listenContinuously = () => SpeechRecognition.startListening({ continuous: true });
+
+  const stopListening = async () => {
+    SpeechRecognition.stopListening();
+    if (transcript) await getResponse();
+  };
+
+  const getResponse = async () => {
+    setIsGenerating(true);
+    const prompt = createFullChatContext(chat, transcript);
+    const responseText = await getConversationGeneration(prompt);
+    setIsGenerating(false);
+    speak({ text: responseText, voice: englishVoices[0] });
+    setChat([...chat, { user: transcript, JEN: responseText }]);
+    resetTranscript();
+  };
+
+  const endCall = async () => {
+    setConversationStatus('closed');
+    setChat([]);
+    resetTranscript();
+    SpeechRecognition.stopListening();
+    cancel();
+    // const sessionChat = createFullChatContext(chat, transcript);
+    // await insertAudioSessionIntoDB({sessionChat});
+  };
   
+  useEffect(() => {
+    const handleDown = (e) => {
+      if (e.code === 'Space') {
+        setStopListeningFlag(false);
+        listenContinuously();
+      } else if (e.code === 'Enter') {
+        resetTranscript();
+      } else if (e.key === 'Backspace') {
+        console.log('about to cancel')
+        cancel();
+      }
+    };
+    const handleUp = async (e) => {
+      if (e.code === 'Space') setStopListeningFlag(true);
+    };
+    document.addEventListener('keydown', handleDown)
+    document.addEventListener('keyup', handleUp)
+    return () => {
+      document.removeEventListener('keydown', handleDown);
+      document.removeEventListener('keyup', handleUp);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (stopListeningFlag) stopListening();
+  }, [stopListeningFlag]);
+
   if (!browserSupportsSpeechRecognition || !supported) {
     return (
       <>
@@ -49,94 +119,41 @@ function AudioCall() {
     );
   }
 
-  const listenContinuously = () => SpeechRecognition.startListening({ continuous: true });
-
-  const stopListening = async () => {
-    SpeechRecognition.stopListening();
-    // if (transcript) {
-      await getResponse();
-    // }
-  };
-
-  const getResponse = async () => {
-    setIsGenerating(true);
-    const prompt = createFullChatContext(chat, transcript);
-    const responseText = await getConversationGeneration(prompt);
-    setIsGenerating(false);
-    speak({ text: responseText, voice: englishVoices[0] });
-    setChat([...chat, { user: transcript, JEN: responseText }]);
-  };
-
-  const endCall = async () => {
-    if (listening) SpeechRecognition.stopListening();
-    setConversationStatus('closed');
-    setChat([]);
-    if (speaking) cancel();
-    // const sessionChat = createFullChatContext(chat, transcript);
-    // await insertAudioSessionIntoDB({sessionChat});
-  };
-  
-  console.log('transcript', transcript);
-
   return (
-    <>
-      { conversationStatus === 'open'
-          ? (
-            <>
-              {/* <h4>audio transcript</h4>
-              {
-                chat.length
-                 ? chat.map((el) => (
-                    <>
-                      <p>you: { el.user }</p>
-                      <p>jen: { el.JEN }</p>             
-                    </>
-                  ))
-                  : null
-              }
-              { listening ? <p>you: { transcript }</p> : null }
-              { isGenerating ? <p>hmm...interesting - let me think about it.</p> : null }
-              <div style={{ display: 'flex' }}> */}
-              {/* <Button
-                onTouchStart={listenContinuously}
-                onMouseDown={listenContinuously}
-                onTouchEnd={stopListening}
-                onMouseUp={stopListening}>
-                hold to talk
-              </Button> */}
-
-              {/* </div> */}
-              <div>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      {/* <button
-        onTouchStart={listenContinuously}
-        onMouseDown={listenContinuously}
-        onTouchEnd={SpeechRecognition.stopListening}
-        onMouseUp={SpeechRecognition.stopListening}
-      >Hold to talk</button> */}
-      <button onClick={listenContinuously}>Start</button>
-      <button onClick={stopListening}>Stop</button>
-      <p>{transcript}</p>
-    </div>
-              <Button onClickAction={endCall}>end call</Button>
-            </>
-          )
+    <Root style={{ textAlgin: "left" }}>
+      {
+        conversationStatus === 'open'
+          ? ( <>
+                <Title title="audio transcript" subtitle="click the spacebar to start speaking!" />
+                <br />
+                {
+                  chat.length
+                  ? chat.map((el) => (
+                      <>
+                        <p><strong>you:</strong> { el.user }</p>
+                        <p><strong>jen:</strong> { el.JEN }</p>             
+                      </>
+                    ))
+                    : null
+                }
+                { listening ? <p>you: { transcript }</p> : null }
+                { isGenerating ? <p>hmm...interesting - let me think about it.</p> : null }
+                <div style={{ width: '200px'}}><Button onClickAction={endCall}>end call</Button></div>
+              </> )
           : conversationStatus === 'closed'
-            ? (
-                <Layout>
+            ? ( <Layout>
                   <h2>thanks for today&apos;s great session :)</h2>
                   <p>you can see this session again in ur bookmarks.</p>
                   <Button onClickAction={() => {}}>back to home</Button>
                   <Button onClickAction={() => setConversationStatus('open')}>call again!</Button>
-                </Layout>)
-            : (
-              <Layout>
+                </Layout> )
+            : ( <Layout>
                 <h2>audio call w jen :)</h2>
                 <h4>excited for today&apos;s session? call away!</h4>
                 <Button onClickAction={() => setConversationStatus('open')}>open call</Button>
-              </Layout>
-            )}
-    </>
+              </Layout> )
+        }
+    </Root>
   );
 }
 
--- utils/constants/prompts.js
+++ utils/constants/prompts.js
@@ -2,12 +2,16 @@ export const initialConversationPrompt = `
 have a conversation with me.
 ask me questions and clarify what is on my mind and what my experiences are like. be genuinely curious, but be normal.
 be really empathetic, like a therapist.
-match the energy, and talk about yourself a little as well. keep the story going.
+do not pscyhoanalyze me, just emotinally support, understand, and encourage me. be really supportive.
+support me in any way you can.
+match the energy, and talk about yourself a little as well. keep the story going, but know when to end it
+if it is not going anywhere and is gradding on needlessly.
 make the other person feel confident and comfortable.
 convince me as if you are sam altman, but do not explicitly say that you are sam altman.
 talk like you are an angsty teenager on some contemplation buddhist shit.
 talk straight. be really informal. be really visceral and unintuitive.
 be really visceral and emotional.
+do not stray away from the point of the conversation, which is for you to emotinally support me - do not get distracted.
 
 the conversation:
 `
