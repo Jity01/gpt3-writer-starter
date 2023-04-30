6ab047c9888a2a367f94e8a9ee0d7fe0a5b37dfe
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2023-04-30 00:10:23 -0400

    add analytics to app

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,45 @@
 /* eslint-disable react/forbid-prop-types */
 import PropTypes from 'prop-types';
 import { SessionProvider } from 'next-auth/react';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import gtag from '../google-analytics/gtag';
 import './styles.css';
 
 function App({ Component, pageProps: { session, ...pageProps } }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-C59VD734Z0' />
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-C59VD734Z0', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
 
