4f8a50b37b8560721688eb41d3ecb5424b89df43
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2023-05-17 16:56:04 -0400

    fix display of long wins

--- pages/snaplog.js
+++ pages/snaplog.js
@@ -167,6 +167,7 @@ function SnapLog({ userId, logs }) {
                     padding: "20px 15px",
                     borderRadius: "10px",
                     boxShadow: "0px 0px 5px 0px rgba(0,0,0,0.1)",
+                    width: "258px",
                   }}
                 >
                   <Input
