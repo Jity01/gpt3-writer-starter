a6ef096b434946fde0203f2ded2725856a873ff7
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2023-06-05 10:14:01 -0400

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import posthog from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react';
 import './styles.css';
 import React from 'react';
+import Head from 'next/head';
 
 if (typeof window !== 'undefined') {
   posthog.init(
@@ -32,11 +33,22 @@ function App({ Component, pageProps: { session, ...pageProps } }) {
     };
   }, []);
   return (
-    <SessionProvider session={session}>
-      <PostHogProvider client={posthog}>
-        <Component {...pageProps} />
-      </PostHogProvider>
-    </SessionProvider>
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <SessionProvider session={session}>
+        <PostHogProvider client={posthog}>
+          <Component {...pageProps} />
+        </PostHogProvider>
+      </SessionProvider>
+    </>
   );
 }
 
